faa417034c166d8560a38983a8b1c764e5e20724
unknown <matin.mtn.mh@gmail.com>
2026-07-21 17:54:01 +0330

    create project config settings

---
+++ app/core/config.py
@@ -0,0 +1,22 @@
+from pydantic_settings import BaseSettings
+
+class Settings(BaseSettings):
+
+    DATABASE_URL: str
+
+    SECRET_KEY: str
+    ALGORITHM: str
+    ACCESS_TOKEN_EXPIRE_MINUTES: int
+
+    AI_API_KEY: str
+    AI_BASE_URL: str
+    AI_MODEL: str
+
+    GITHUB_TOKEN: str | None = None
+
+    class Config:
+        env_file = ".env"
+        extra = "ignore"
+
+
+settings = Settings()
