8387cb27f6f7a38bbbae9f31580727c2e472c0fb
unknown <matin.mtn.mh@gmail.com>
2026-07-23 18:20:51 +0330

    update project README

--- app/main.py
+++ app/main.py
@@ -7,7 +7,6 @@ from app.api.reviews import router as review_router
 
 from app.models.user import User
 from app.models.review import Review
-from app.models.job import Job
 
 Base.metadata.create_all(bind=engine)
 app = FastAPI(title= "AI Code Review API",)
