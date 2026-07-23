6fde6108203471272d9f133d7b8b8ef3faf07675
unknown <matin.mtn.mh@gmail.com>
2026-07-23 18:31:20 +0330

    update auth endpoints

--- app/api/auth.py
+++ app/api/auth.py
@@ -4,6 +4,8 @@ from app.schemas.user import UserResponse, UserCreate, UserLogin, TokenResponse
 from app.database.database import get_db
 from app.services.auth_service import AuthService
 
+from fastapi.security import OAuth2PasswordRequestForm  # Add this import
+
 router = APIRouter(prefix="/auth", tags=["Authentication"])
 
 
@@ -15,7 +17,12 @@ def get_auth_services(db: Session = Depends(get_db)):
 def register(user_data: UserCreate, service: AuthService = Depends(get_auth_services)):
     return service.register_user(user_data)
 
-@router.post("/login", response_model=TokenResponse)
-def login(user_data: UserLogin, service: AuthService = Depends(get_auth_services)):
-    return service.login_user(user_data)
 
+@router.post("/login", response_model=TokenResponse)
+def login(form_data: OAuth2PasswordRequestForm = Depends(), service: AuthService = Depends(get_auth_services)):
+
+    user_data = UserLogin(
+        username=form_data.username,
+        password=form_data.password
+    )
+    return service.login_user(user_data)
